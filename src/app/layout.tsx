33a5e9178093b7abccfe03b617edd0d30d681fc2
Samadrita Bhattacharya <samadritabhatt163.official@gmail.com>
2024-07-05 21:34:07 +0530

    cropped image saveed and db updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "@/components/Nabar";
 import Footer from "@/components/Footer";
 import { Toast } from "@/components/ui/toast";
 import { Toaster } from "@/components/ui/toaster";
+import Providers from "@/components/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,7 +25,9 @@ export default function RootLayout({
         <Navbar />
         <main className=" flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
           <div className=" flex-1 flex flex-col h-full">
-            {children}
+            <Providers>
+              {children}
+            </Providers>
           </div>
 
           <Footer />
