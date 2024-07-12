90beb672e23860722dd36e96d4a83b03f3872780
Samadrita Bhattacharya <samadritabhatt163.official@gmail.com>
2024-07-12 19:14:35 +0530

    button loading and metadata added.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
+import { Metadata } from "next"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -17,3 +18,34 @@ export const formatPrice = (price: number) => {
 }
 
 
+export function constructMetadata({
+  title = 'CaseaDree - custom high-quality phone cases',
+  description = 'Create custom high-quality phone cases in seconds',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@Samadrita163',
+    },
+    
+    icons,
+    metadataBase: new URL("https://caseadree.vercel.app/")
+  }
+}
