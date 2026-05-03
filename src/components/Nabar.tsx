5569be57839049e72052d0499954b92278eb4170
Samadrita Bhattacharya <samadritabhatt163.official@gmail.com>
2026-05-03 20:10:11 +0530

    updated

--- src/components/Nabar.tsx
+++ src/components/Nabar.tsx
@@ -12,8 +12,8 @@ const Navbar = async () => {
 
   return (
     <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
-      <MaxWidthWrapper>
-        <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
+      {/* <MaxWidthWrapper> */}
+        <div className='flex h-14 items-center justify-between border-b border-zinc-200 px-2.5 md:px-16'>
           <Link href='/' className='flex z-40 font-semibold'>
             casea<span className='text-green-600'>Dree</span>
           </Link>
@@ -84,7 +84,7 @@ const Navbar = async () => {
             )}
           </div>
         </div>
-      </MaxWidthWrapper>
+      {/* </MaxWidthWrapper> */}
     </nav>
   )
 }
